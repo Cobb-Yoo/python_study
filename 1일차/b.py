cdf1aaf27038348e3eb892828c02f5732bec5ecf
YooCobb <yhj010597@naver.com>
2021-02-17 22:37:03 +0900

    1일차

---
+++ 1일차/b.py
@@ -0,0 +1,25 @@
+a = 'Hello world'
+b = "Hello' world"
+c = '''Hello' world'''
+d = """Hello
+    world"""
+
+print(a)
+print(b)
+print(c)
+print(d)
+
+
+#indexing
+
+e = "Life is too short, you need Go?"
+
+print(e[:8:2])
+
+#formating
+
+f = "asdasdasd. {} ??".format("hello")
+
+print(f)
+
+#
